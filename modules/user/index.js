598c4e1e0041541f7ab56130f4879dafbeaf4886
Alex Turuta <aturuta@servicechannel.com>
2015-12-16 19:23:41 +0300

    user module. dir/index implementation

---
+++ modules/user/index.js
@@ -0,0 +1,18 @@
+//exports is result of require
+var phrases = require('./ru');
+
+function User(name){
+    this.name = name;
+};
+
+User.prototype.hello = function (who) {
+    console.log(phrases.Hello + ", " + who.name);
+};
+
+//...
+
+exports.User = User;
+
+
+// global.User = User
+// global - object for globalization variables
