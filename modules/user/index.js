faf87d6ab3dea9f618dd4675f269d52acdeedf9c
Alex Turuta <aturuta@servicechannel.com>
2015-12-23 15:49:44 +0300

    module-fabric

--- modules/user/index.js
+++ modules/user/index.js
@@ -1,12 +1,13 @@
 //exports is result of require
 var db = require('db');
+var log = require('logger')(module);
 
 function User(name){
     this.name = name;
 };
 
 User.prototype.hello = function (who) {
-    console.log(db.getPhrase('Hello') + ", " + who.name);
+    log(db.getPhrase('Hello') + ", " + who.name);
 };
 
 //...
