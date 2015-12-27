866bbfab402ba432b816227db276e46466ac5594
Alex Turuta <alexandr.turuta@gmail.com>
2015-12-27 16:15:20 +0300

    logging with 'debug'

---
+++ logging/server.js
@@ -0,0 +1,10 @@
+var http = require('http');
+var debug = require('debug')('server');
+
+var server = new http.createServer();
+
+server.on('request', require('./request'));
+
+server.listen(1337);
+
+debug('Server is running');
