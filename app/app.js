8469bedd442b5a2c68809fafd5c9af785667dba5
Alex Turuta <alexandr.turuta@gmail.com>
2016-01-06 13:50:15 +0300

    site skeleton

---
+++ app/app.js
@@ -0,0 +1,108 @@
+var express = require('express');
+
+var app = express();
+app.set('port', 3000);
+
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var errorHandler = require('errorhandler');
+
+
+app.use(function (req, res, next) {
+  if(req.url == '/'){
+    res.end('Hello');
+  } else {
+    next();
+  }
+});
+
+app.use(function (req, res, next) {
+  if(req.url == '/index'){
+    res.end('index');
+  } else{
+    next()
+  }
+});
+
+app.use(function (req, res, next) {
+  if(req.url == '/forbidden'){
+    next(new Error('oops, denied'))
+  } else{
+    next()
+  }
+});
+
+app.use(function (req, res) {
+  res.status(404)
+      .send('Page Not Found')
+});
+
+if(app.get('env') == 'development'){
+  app.use(errorHandler());
+}
+/*app.use(function (err, req, res, next) {
+  if(app.get('env') == 'development'){
+    errorHandler();
+  } else {
+    res.status(500).send('Internal Server Error');
+  }
+});*/
+
+/*
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(require('less-middleware')(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+*/
+app.listen(app.get('port'));
+
+module.exports = app;
