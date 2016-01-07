27db586bc3a67273b489131c61e850f7f4582c67
Alex Turuta <alexandr.turuta@gmail.com>
2016-01-07 04:53:31 +0300

    mongoose

--- app/createDb.js
+++ app/createDb.js
@@ -1,26 +1,14 @@
-var MongoClient = require('mongodb').MongoClient
-    , assert = require('assert');
+var User = require('./models/user').User;
 
-// Connection URL
-var url = 'mongodb://localhost:27017/chat';
-// Use connect method to connect to the Server
-MongoClient.connect(url, function(err, db) {
+var user = new User({
+    username: 'Tester1',
+    password: 'secret'
+});
+
+user.save(function (err, user, affected) {
     if(err) throw err;
-
-    var c = db.collection('test_insert');
-    c.remove({}, function(err, items){
-        console.log(arguments)
-        c.insert({a:2}, function(err, docs){
-            if(err) throw err;
-
-            var cursor = c.find({a:2});
-            cursor.toArray(function (err, res) {
-                console.dir(res);
-
-                db.close();
-            });
-        });
+    User.findOne({username: 'Tester'}, function (err, res) {
+        if(err) throw err;
+        console.log(res);
     })
-
-
 });
