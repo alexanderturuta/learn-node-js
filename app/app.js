deac370ae68fd9052fafcb9758f3c864ca8542fa
Alex Turuta <alexandr.turuta@gmail.com>
2016-01-11 04:46:23 +0300

    socket io

--- app/app.js
+++ app/app.js
@@ -6,6 +6,7 @@ var session = require('express-session');
 var mongoose = require('./libs/mongoose');
 
 var app = express();
+var http = require('http');
 var logger = require('morgan');
 var isDevelopment = app.get('env') == 'development';
 // view engine setup
@@ -83,47 +84,20 @@ if(isDevelopment){
   })
 }
 
-/*
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
-
-app.use('/', routes);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-*/
-app.listen(config.get('port'), function () {
+var server = http.createServer(app);
+server.listen(config.get('port'), function () {
   console.log('Listening on port: %s', config.get('port'));
 });
+
+var io = require('socket.io').listen(server);
+
+io.sockets.on('connection', function (socket) {
+    socket.on('message', function (text, cb) {
+        console.log(text)
+        socket.broadcast.emit('message', text);
+        cb('data');
+    });
+});
+
 
 module.exports = app;
